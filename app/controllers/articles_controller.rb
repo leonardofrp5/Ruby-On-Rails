87757eeb5f8eb60793bf3a824d073897cdf13731
leonardo <leonardofrp@hotmail.com>
2018-07-24 21:48:32 -0500

    se crea el metodo de eliminar

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,17 +3,41 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
   #get /articles/:id
   def show
-    @articles = Article.find(params[:id])
+    @article = Article.find(params[:id])
+    Article.where.not("id = ?", params[:id])
   end
+
   #get /articles/new
   def new
-    @articles = Article.new
+    @article = Article.new
   end
+
   #post /articles
   def create
-    @articles = Article.new(title: params[:artilce][:title],
-                            body: params[:artilce][:body])
+    @article = Article.new(title: params[:article][:title],
+                            body: params[:article][:body])
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
   end
+
+  #get /articles/:id/uptade
+  def update
+
+  end
+
+  #
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
 end
